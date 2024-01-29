d2a8c92c5f7cf7465a9d265ef3e2ed39e6ad8469
benjaminfarias313 <benjaminfarias313@gmail.com>
2024-01-29 12:41:43 -0800

    added and removed comments where warranted, changed if to cond and added parenthesis where needed

--- Homework1.lisp
+++ Homework1.lisp
@@ -1,63 +1,37 @@
-
+;;Problem 1
 (defun intmax 
     (x y)
-    (if
+    (cond (
     ;;if x is less than y then print y
         (< x y)
         (print 
-            (y))
+            (y)))
             ;; otherwise print x
         (print 
             (x))))
-
+;;Problem 2
 (defun dup 
     (x)
     (print '
         (x,x)))
 
-
+;;Problem 3
 (defun factorialSum 
     (x) 
-    (if 
+    (cond 
     ;; if x is 1 then return 1 
-        (= x 1) 
-        (1)
+        ((= x 1) 
+        (1))
         ;; otherwise add x and recursively call the function
         (x + factorialSum
             (x-1)) ))
 
-;;Problem4
-;;Writeafunction,backwards,toreturnsalistwithareversedorderlistoftheelements.
-;;Thefunctionisasimpleversionoftheproblemwhereonlyworkontheelementsofthelist
-;;andnotofanysublists!
+;;Problem 4
+(defun backwards)
 
-;;Example:
-;;(backwards 
-    (quote 
-        (a b c h i)))
-;;(I H C B A)
 
 ;;Problem5
-;;Writeafunction,totalbackwards,toreturnsalistwithareversedorderlistoftheelements.
-;;Inthisversionoftheproblemisnecessaryreversertheelementsofthelistandthesublists.
-;;Example:
-;;(totalbackwards 
-    (quote 
-        (a 
-            (b c) 
-            (
-                (l k 
-                    (t)) h i))))
-;;(                (I H 
-        (
-            (T) K L)) 
-    (C B) A)
+(defun totalBackwards)
 
 ;;Problem6
-;;Analgorithmtodetectpalindromesissimpleprocesswithindicesiandjatthefirstandlastlettersofthewordandcomparethetwoletters.Iftheyaredifferent,youcanstoprighttherebecausethewordisnotapalindrome,otherwiseattheendoftheprocessthewordisapalindrome.Writeafunction,palindrome,thattakesalistasinputandreturnsTifthelistisapalindrome.
-;;(maybe the last algorithm is the most efficient but difficult to implement in LISP)
-;;Example:
-;;(palindrome 
-    (quote 
-        (r o t o r)))
-;;(T)
+(defun palindrome)
